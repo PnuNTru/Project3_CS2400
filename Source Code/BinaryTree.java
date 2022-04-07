1ac8b1d3604df4c5abbd5f536a5c6ea2cf978dcc
Tophuu <95060057+Tophuu@users.noreply.github.com>
2022-04-06 21:24:41 -0700

    updated method

--- Source Code/BinaryTree.java
+++ Source Code/BinaryTree.java
@@ -146,7 +146,7 @@ public class BinaryTree<T> implements BinaryTreeInterface<T>
    @return  The height of the "whole" tree. */
    public int getHeight_callBinaryNodeMethod()
    {
-	   return 0;
+	   return root.getHeight_binaryNodeMethod();
    } // end getHeight_callBinaryNodeMethod
 
    /** -------------------------------------------------------------------- */
